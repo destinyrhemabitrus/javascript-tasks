6cd858639650259dd261061639cb77bd653c989d
destinyrhemabitrus <destinyrhemabitrus@gmail.com>
2026-05-18 09:18:18 +0100

    update

---
+++ easterEggs/meatPie/age.js
@@ -0,0 +1,3 @@
+const prompt = require("prompt-sync")();
+const age = prompt("How old are you? ")
+console.log("You are " + age + " years old")
---
+++ easterEggs/meatPie/star_triangle.js
@@ -0,0 +1,18 @@
+//for(let counter = 1; counter <= 4; counter + 1){
+//
+//    for(let count = 1; count <= counter; count + 1){
+//
+//        console.log("* ");
+//    
+//    }
+//    console.log();
+//
+//}
+let output = "";
+for(let counter = 1; counter <= 5; counter++){
+
+    output = output + counter;
+ 
+
+}
+   console.log(output);
